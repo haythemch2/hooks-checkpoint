b2fb5e3df9e8e1556d79e74edaca1c7b5658b49a
haythemch2 <81178984+haythemch2@users.noreply.github.com>
2021-04-22 14:33:49 +0200

    Add files via upload

--- src/Components/Cardcontainer.js
+++ src/Components/Cardcontainer.js
@@ -2,20 +2,22 @@ import React from "react";
 import {  Card } from "react-bootstrap";
 
 function Cardcontainer(props) {
-
+     let Searchcondition =props.Conditionsearch
+     let searchedmovielist=props.list.filter((x)=>(x.rating>=Searchcondition.rating&&x.name.toLowerCase().includes(Searchcondition.name.toLowerCase())))
+    console.log(Searchcondition) 
+    console.log(searchedmovielist)
     return (
-
-      
         <div className="Cardcontainer">
-      {props.list.map((movie)=>
+      {searchedmovielist.map((movie)=>
       <Card className="bg-dark text-white" style={{ width: "30rem" }}>
       <Card.Img
         src={movie.posterurl}
         alt="Card image"
+        style={{filter: 'blur(1px)'}}
       />
       <Card.ImgOverlay>
-        <Card.Title>{movie.name}</Card.Title>
-        <Card.Text>
+        <Card.Title style={{ textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black'}}>{movie.name}</Card.Title>
+        <Card.Text style={{ textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black'}}>
           {movie.description}
         </Card.Text>
         <Card.Text>rating : {movie.rating} Stars</Card.Text>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import {Navbar,Nav} from 'react-bootstrap'
+import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap'
 
-function NavMenu({onChange}) {
+function NavMenu({onChange,searchcondition,setSearchcondition}) {
   const addpage = ()=>{
     onChange('addpage')
   }
   const homepage = ()=>{
     onChange('home')
   }
-
+  let search ={name:'',rating:0}
+  const handleSearch=()=>{
+    setSearchcondition(search)
+    console.log(search)
+  }
     return (
         <div>
             <Navbar bg="dark" variant="dark">
@@ -18,6 +22,11 @@ function NavMenu({onChange}) {
       <Nav.Link href="#Movies" onClick={homepage}>Movies</Nav.Link>
       <Nav.Link href="#Series" onClick={addpage}>Add a Movie</Nav.Link>
     </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Movie name" className="mr-sm-2" onChange={x=>search.name=x.target.value}/>
+      <Form.Control type='number' placeholder='rating' onChange={x=>search.rating=x.target.value} ></Form.Control>
+      <Button onClick={handleSearch}>Search</Button>
+    </Form>
   </Navbar>
         </div>
     )
